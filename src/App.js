46b387774e1b5fd01d72033ea6514e0f770e2542
Kevin Okoth <kevinokotth04@gmail.com>
2023-04-17 12:08:26 +0300

    Add browser routes

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="/" index element={<Home />} />
+        <Route path="details" element={<Details />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;

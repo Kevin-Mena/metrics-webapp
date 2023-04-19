aa91b5cec8e81c30da8784e29f3cbab03cf803ea
Kevin Okoth <kevinokotth04@gmail.com>
2023-04-19 14:20:05 +0300

    Remove unused files

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countriesReducer from './redux/countriesSlice';
-import detailsReducer from './redux/detailsSlice';
 
 const store = configureStore({
   reducer: {
     countries: countriesReducer,
-    details: detailsReducer,
   },
 });
 
